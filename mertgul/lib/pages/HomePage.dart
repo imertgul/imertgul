563ed987987ec02249d8adf193bf6e80d0c3e5b5
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-14 13:46:16 +0300

    homapage background image fitted

---
+++ mertgul/lib/pages/HomePage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mertgul/utilities/constant.dart';
+import '../widgets/repositories.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/background.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[],
+          )
+        ],
+      ),
+    );
+  }
+}
