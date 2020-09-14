edbf8ff8ecc68b2fe9a29dee8a4f0e5cbe7153ec
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-14 17:57:47 +0300

    main page containers

--- mertgul/lib/pages/HomePage.dart
+++ mertgul/lib/pages/HomePage.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
+        children: <Widget>[
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
@@ -21,14 +21,153 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(200.0),
-            child: Center(
-              child: buildRepos(),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildProfile(),
+                _buildExperience(),
+                _buildRepo(),
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
   }
+}
+
+Widget _buildProfile() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 25),
+    child: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Renkler.beyaz70,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Profile",
+                  style: styleBaslik,
+                ),
+                Text(
+                  "detailed",
+                  style: styleLight,
+                )
+              ],
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _buildExperience() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 25),
+    child: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Renkler.beyaz70,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Experience",
+                  style: styleBaslik,
+                ),
+                Text(
+                  "detailed",
+                  style: styleLight,
+                )
+              ],
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _buildRepo() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 25),
+    child: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Renkler.beyaz70,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "Repositories",
+                  style: styleBaslik,
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "Detailed information about open source projects",
+                      style: styleLight,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Divider(),
+            Container(height: 500, child: buildRepos())
+          ],
+        ),
+      ),
+    ),
+  );
 }
