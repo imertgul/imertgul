6fc21cd66dd0c0d353fc3990b59ca2e2969fe4a1
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-14 13:46:00 +0300

    flutter web for mertgul.com.tr init

---
+++ mertgul/android/app/src/main/kotlin/com/example/mertgul/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mertgul
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mertgul/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import './pages/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My home Page',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
